824a4748ce9316680661287e39bb928921377d4a
ElifSinemAktas <elifsinem.aktas@gmail.com>
2022-08-24 16:56:29 +0300

    Add sqlite path as alternative

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Bootstrap(app)
 ## CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 ## CHANGE DATABASE
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
